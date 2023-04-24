98e160eaf60773ae5588ccf2f9cb1b856678fe35
satishkumar2232 <130146205+satishkumar2232@users.noreply.github.com>
2023-04-24 23:53:01 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -84,7 +84,6 @@ const Post: React.FC<PostProps> = (props) => {
 			<Head>
 				<meta property="og:title" content={post.title} />
 				<link rel="canonical" href={`https://${host}/${path}`} />
-				
 				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
